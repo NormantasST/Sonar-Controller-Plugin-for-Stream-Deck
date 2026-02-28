2c78f73766cf1a167823ad4db3b6ae0ad51917b5
Normantas <normantas.stankevicius@gmail.com>
2026-02-28 11:57:50 +0200

    Fix initialization bug

--- src/sonar-helper.ts
+++ src/sonar-helper.ts
@@ -1,4 +1,4 @@
-import streamDeck from '@elgato/streamdeck';
+import streamDeck, { Device } from '@elgato/streamdeck';
 import sonarClient from './services/sonar-client';
 import { RedirectionEnum, StreamRedirectionEnum } from './models/types/sonar-models.type';
 import { GlobalSettings } from './models/types/global-settings.type';
@@ -14,39 +14,39 @@ export async function getCurrentSonarSettings(): Promise<GlobalSettings>
     const mode = await sonarClient.getSonarModeAsync();
 
     const allOutputDevices = await sonarClient.getAllOutputAudioDevicesAsync();
-    const gameDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Game)!.deviceId);
-    const chatDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Chat)!.deviceId);
-    const mediaDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Media)!.deviceId);
-    const auxDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Aux)!.deviceId);
+    const gameDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Game)?.deviceId);
+    const chatDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Chat)?.deviceId);
+    const mediaDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Media)?.deviceId);
+    const auxDevice = allOutputDevices.find((x: { id: any; }) => x.id == classicRedirections.find((x) => x.id == RedirectionEnum.Aux)?.deviceId);
 
-    const personalDevice = allOutputDevices.find((x: { id: any; }) => x.id == streamRedirections.find((x) => x.streamRedirectionId == StreamRedirectionEnum.PersonalMix)!.deviceId);
-    const streamMixDevice = allOutputDevices.find((x: { id: any; }) => x.id == streamRedirections.find((x) => x.streamRedirectionId == StreamRedirectionEnum.StreamMix)!.deviceId);
+    const personalDevice = allOutputDevices.find((x: { id: any; }) => x.id == streamRedirections.find((x) => x.streamRedirectionId == StreamRedirectionEnum.StreamMix)?.deviceId);
+    const streamMixDevice = allOutputDevices.find((x: { id: any; }) => x.id == streamRedirections.find((x) => x.streamRedirectionId == StreamRedirectionEnum.StreamMix)?.deviceId);
 
     const globalSettings: GlobalSettings = {
         sonarMode: mode,
         gameChannel: {
-            deviceId: gameDevice!.id!,
-            deviceName: gameDevice!.friendlyName,
+            deviceId: gameDevice?.id ?? 'Unknown',
+            deviceName: gameDevice?.friendlyName ?? 'Unknown',
         },
         chatChannel: {
-            deviceId: chatDevice!.id!,
-            deviceName: chatDevice!.friendlyName,
+            deviceId: chatDevice?.id ?? 'Unknown',
+            deviceName: chatDevice?.friendlyName ?? 'Unknown',
         },
         mediaChannel: {
-            deviceId: mediaDevice!.id!,
-            deviceName: mediaDevice!.friendlyName,
+            deviceId: mediaDevice?.id ?? 'Unknown',
+            deviceName: mediaDevice?.friendlyName ?? 'Unknown',
         },
         auxChannel: {
-            deviceId: auxDevice!.id!,
-            deviceName: auxDevice!.friendlyName,
+            deviceId: auxDevice?.id ?? 'Unknown',
+            deviceName: auxDevice?.friendlyName ?? 'Unknown',
         },
         personalMixChannel: {
-            deviceId: personalDevice!.id!,
-            deviceName: personalDevice!.friendlyName!
+            deviceId: personalDevice?.id ?? 'Unknown',
+            deviceName: personalDevice?.friendlyName ?? 'Unknown',
         },
         streamMixChannel: {
-            deviceId: streamMixDevice!.id!,
-            deviceName: streamMixDevice!.friendlyName!
+            deviceId: streamMixDevice?.id ?? 'Unknown',
+            deviceName: streamMixDevice?.friendlyName ?? 'Unknown',
         }
     };
 
